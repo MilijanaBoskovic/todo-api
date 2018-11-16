5e21d141eb4d5981fcc4cf69e9798ea254f471fe
unknown <alejna94@gmail.com>
2018-11-16 16:58:43 +0100

    before last video

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-var express=require('express');
-var bodyParser=require('body-parser');
-const path = require('path')
-var {ObjectID}=require('mongodb');
+const express=require('express');
+const bodyParser=require('body-parser');
+const path = require('path');
+const {ObjectID}=require('mongodb');
+const _=require('lodash');
+
+
 var {mongoose}= require('./db/mongoose.js')
 var {Todo} =require('./models/todo.js');
 var {User} =require('./models/user.js');
@@ -51,6 +54,58 @@ app.get('/todos/:id',(req,res)=>
   });
 
 });
+
+
+app.delete('/todos/:id',(req,res)=>{
+
+  var id=req.params.id;
+
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo)=>
+  {
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+
+  var id=req.params.id;
+  var body= _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed)
+  {
+    body.completedAt=new Date().getTime();
+  }
+  else
+  {
+    body.completed=false;
+    body.completedAt=null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{ res.status(400).send()});
+
+});
+
 
 app.listen(port,()=>{
   console.log(`Server started on port ${port}`);
